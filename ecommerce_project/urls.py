446d42222aed78a6e9fa5b4a60c4416c4c892f3f
Olayinka <olaleyeolayinka897@gmail.com>
2025-03-12 23:47:41 +0100

    Static files added

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.views.i18n import set_language
 from django.conf.urls.static import static  
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -37,6 +38,7 @@ urlpatterns += i18n_patterns(
      
       path('i18n/setlang/', set_language, name='set_language'),
 )
+urlpatterns += staticfiles_urlpatterns()
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)  
